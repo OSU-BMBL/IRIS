89030d466bca0f36e1c8a55fc9a3bfe5597cc9eb
btmonier <brandon.monier@gmail.com>
2018-03-14 17:27:18 -0500

    Use new package load script
    Former-commit-id: 05c61f39ecb175d6e4299a33ca4ad20db7406444 [formerly f5534ea332bc7b293d453a7113a97d4c6e940531]
    Former-commit-id: 432908c5a0c3380b7a85f5185f6c9090073f364f

--- app.R
+++ app.R
@@ -5,34 +5,17 @@
 # Last Modified: 2018-01-26 at 11:31:06
 #---------------------------------------------------------------------
 
-# Package logic ----
-
 ## Set working directory (FOR LOCAL TESTING ONLY)
-# setwd("D:/Box Sync/misc-github-repos/iris/")
-
-## CRAN
-source("iris-functions.R")
-packages <- c(
-	"crosstalk", "dplyr", "DT", "gtools", "plotly", "shiny", "plyr",
-	"shinyBS", "shinycssloaders", "shinythemes", "tibble", "tidyr",
-	"Rcpp", "Hmisc", "ggplot2", "geneplotter", "locfit", "GGally", 
-	"pheatmap",	"reshape2", "backports", "digest", "fields", "psych"
-)
-pack.man(packages)
-
-## Bioconductor
-source("https://bioconductor.org/biocLite.R")
-if (!require("DESeq2")) biocLite("DESeq2")
-if (!require("edgeR")) biocLite("edgeR")
-if (!require("limma")) biocLite("limma")
-if (!require("QUBIC")) biocLite("QUBIC")
+setwd("D:/Box Sync/misc-github-repos/iris/")
 
 
+# Load packages ----
+source("pack-load.R")
 
-# Sources ----
+
+# Server sources ----
 source("irisUI.R")
 source("irisServer.R")
-
 
 
 # Run shiny ----
