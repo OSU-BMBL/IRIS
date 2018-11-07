d4f42ea78fc387498e679bcfde9bbcc346636901
Brandon Monier <brandon.monier@gmail.com>
2018-11-07 11:42:31 -0500

    Add packages

--- pack-load.R
+++ pack-load.R
@@ -2,7 +2,7 @@
 # Title:         IRIS - Package Loader
 # Author:        Brandon Monier
 # Created:       2018-03-14 at 17:02:31
-# Last Modified: 2018-11-06 at 13:06:02
+# Last Modified: 2018-11-07 at 10:35:54
 #---------------------------------------------------------------------
 
 packages <- c(
@@ -11,7 +11,7 @@ packages <- c(
 	"Rcpp", "Hmisc", "ggplot2", "geneplotter", "locfit", "GGally", 
 	"pheatmap",	"reshape2", "backports", "digest", "fields", "psych",
 	"DESeq2", "edgeR", "limma", "QUBIC", "stringr", "tools", "openxlsx",
-	"Rtsne", "WGCNA", "flashClust", "parallel"
+	"Rtsne", "WGCNA", "flashClust", "parallel", "MCL", "kmed"
 )
 
 lapply(packages, require, character.only = TRUE)
